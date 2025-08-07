6f9eeb3efb554e7794df9b9b2b6950d9428bb9c6
𝒫𝑒𝒹𝓇𝑜 ℛ𝓈𝓈𝒶𝓉𝑜𝑜𝓃 <rossato.pedro@escola.pr.gov.br>
2025-08-07 14:33:17 -0300

    Teste

--- suporte.js
+++ suporte.js
@@ -1,18 +1,3 @@
-// Configuração do Firebase
-var firebaseConfig = {
-  apiKey: "AIzaSyAEDs-1LS6iuem9Pq7BkMwGlQb14vKEM_g",
-  authDomain: "chatboot--coamo.firebaseapp.com",
-  databaseURL: "https://chatboot--coamo-default-rtdb.firebaseio.com",
-  projectId: "chatboot--coamo",
-  storageBucket: "chatboot--coamo.appspot.com",
-  messagingSenderId: "328474991416",
-  appId: "1:328474991416:web:cd61d9ac5377b6a4ab3fcd",
-  measurementId: "G-4QH32PWFM4"
-};
-
-// Inicializa o Firebase
-firebase.initializeApp(firebaseConfig);
-
 // Referência ao banco de dados
 const db = firebase.database();
 const messagesRef = db.ref("mensagens");
